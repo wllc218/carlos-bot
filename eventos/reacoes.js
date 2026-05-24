52b1675f395aaffb8e1fe285fc7c28ca3c0d4c88
Robi6k <Roobboor6@gmail.com>
2026-05-23 23:08:50 -0300

    nun

--- eventos/reacoes.js
+++ eventos/reacoes.js
@@ -8,13 +8,13 @@ module.exports = {
         if (message.author.bot) return;
 
         const reacoes = {
-            "<:rob:1390110593721040966>": ["nicolas", "rob", "robique", "negro", "nigga", "nigger", "preto", "4", "quatro"],
-            "<:Lucas:1390114953582870833>": ["lucas", "prjooj", "luque", "3", "três"],
-            "<:wallace:1390110948127146086>": ["wallace", "walface", "cabeça", "2", "dois"],
-            "<:carlos:1390110742971289632>": ["carlos", "deyvid", "1", "um"],
+            "<:rob:1390110593721040966>": ["nicolas", "rob", "robique", "negro", "nigga", "nigger", "preto", "4", "quatro", "Four"],
+            "<:Lucas:1390114953582870833>": ["lucas", "prjooj", "luque", "3", "três", "three"],
+            "<:wallace:1390110948127146086>": ["wallace", "walface", "cabeça", "2", "dois", "two"],
+            "<:carlos:1390110742971289632>": ["carlos", "deyvid", "1", "um", "one"],
             "<:theo:1390110538993766421>": ["theo"],
             "<:vocejaviuessameniuna:1415817551333818550>": ["nos", "agente", "vamos", "a gente", "vamo", "gente", "iremos", "somos", 
-                "nossas", "5", "cinco"],
+                "nossas", "5", "cinco", "five"],
             "<:vagina:1390109875089965228>": ["regina", "vagina"],
             "<:oie:1497972408391434462>": ["ele", "eles", "ela", "elas", "TAKE THIS", "TAKE THAT", "LETS DO THIS", "YES", "HAH[AH]*",
                 "TAKE", "CAPUHA", "PAPUHA", "PREPARE FOR", "NIGGARLAS"
