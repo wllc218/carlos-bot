dd01c1e0322bdcec9dc33bccfb70b87b88b78066
wllc218 <wallacealencar636@gmail.com>
2026-05-24 15:37:17 -0300

    PRINTGUESS

--- comandos/veremoji.js
+++ comandos/veremoji.js
@@ -1,41 +1,39 @@
 const { EmbedBuilder } = require("discord.js");
 const listaEmojis = require("../data/emojis.json");
-const lista = require("./lista");
+
 module.exports = {
 
     name: "veremoji",
     execute(message, args) {
-        const emojiS = listaEmojis.emojis;
-        let prikito;
 
-        if (args.length === 0) {
-            message.reply("VSF VC NAO ESCERVEU NADA NA FRENTE ");
-            return;
+        if (!args[0]) {
+            return message.reply("VC NAO ESCREVEU NADA");
         }
 
-        if (args > listaEmojis.emojis.length || args < 0) {
-            message.reply("NN TEM ESSE EMOJI AI NAO ESCOLHE UM NUMERO ENTRE 0 E " + (listaEmojis.emojis.length - 1));
-            return;
-        }
+        const input = args[0].toLowerCase();
 
+        let emoji;
 
-
-       const embedEmoji = new EmbedBuilder()
-            .setTitle(emojiS[args].nome)
-            .setDescription(`${mensagem.join("\n")}`)
-            .setColor(`#000000`)
-            .setFooter({ text: "oi" })
-            .setTimestamp()
-            
-
-
-        if (isNaN(args[0])) {
-            prikito = "penis";
+        if (!isNaN(input)) {
+            emoji = listaEmojis.emojis.find(e => e.id == input);
         } else {
-            prikito = "buceta";
+            emoji = listaEmojis.emojis.find(e => e.nome.toLowerCase() === input);
         }
 
-
-        message.reply(emojiS[args].nome);
+        if (!emoji) {
+            return message.reply(`NN TEM ESSE EMOJI AI NAO ESCOLHE UM NUMERO ENTRE 0 E ${listaEmojis.emojis.length - 1} OU ESCREVE O NOME CERTO NE BURRAO`);
         }
+
+        const embed = new EmbedBuilder()
+            .setTitle(emoji.nome)
+            .setDescription(
+                `**RARIDADE**: ${emoji.raridade}\n` +
+                `**CONTADOR**: ${emoji.contador}\n` +
+                `**ID**: ${emoji.id}`
+            )
+            .setColor("#000000")
+            .setThumbnail(`https://cdn.discordapp.com/emojis/${emoji.discordId}.png`)
+
+        message.reply({ embeds: [embed] });
+    }
 };
