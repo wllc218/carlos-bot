810391669dee1cb890499a7cf73e7c413571f8c5
wllc218 <wallacealencar636@gmail.com>
2026-07-03 05:26:06 -0300

    teste2

--- server/mongo.js
+++ server/mongo.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export default async () => {
+  console.log("MONGO_URI:", process.env.MONGO_URI);
+
   await mongoose.connect(process.env.MONGO_URI);
-  return mongoose;
 };
