00c19ebd974c51550400f7c6ec3a2c90e42a69ad
Rob6 <roobbor6@gmail.com>
2026-07-09 14:09:57 -0300

    oi achei sim

--- server/mongo.js
+++ server/mongo.js
@@ -1,7 +1,6 @@
 import dns from "node:dns";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
-
 import mongoose from "mongoose";
 export default async () => {
   await mongoose.connect(process.env.MONGO_URI);
