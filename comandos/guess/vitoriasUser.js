1dd4c39273f7aa69f46286f9883c1fc9d4bbde07
wllc218 <wallacealencar636@gmail.com>
2026-07-09 20:25:58 -0300

    oi

--- comandos/guess/vitoriasUser.js
+++ comandos/guess/vitoriasUser.js
@@ -1,8 +1,8 @@
-import User from "../../server/schemas/user-schema.js";
+import User from "../../server/schemas/user-schema";
 export const name = "vitoria";
 export async function execute(message) {
   const user = await User.findById(message.author.id);
   message.reply(
-    `**DIALOGUESSER** = ${user.vitorias.dialogo}\n**PRINTGUESSER** = ${user.vitorias.print}`,
+    `**DIALOGUESSER** = ${user.vitorias.dialoguess}\n**PRINTGUESSER** = ${user.vitorias.printguess}`,
   );
 }
