c24153277bc1ad5f13f297ab6c4ee218d1e7236a
wllc218 <wallacealencar636@gmail.com>
2026-07-09 22:19:32 -0300

    hello mario

---
+++ comandos/guess/vitorias.js
@@ -0,0 +1,9 @@
+import User from "../../server/schemas/user-schema.js";
+export const name = "vitoria";
+export async function execute(message) {
+  const oi = await User.findById(message.author.id);
+  const user = oi.vitorias;
+  message.reply(
+    `**NUMEROGUESSER: **${user.numeroGuess}\n**DIALOGUESSER: **${user.dialoGuess}\n**PRINTGUESSER: **${user.printGuess}\n`,
+  );
+}
--- comandos/guess/vitoriasUser.js
+++
@@ -1,6 +0,0 @@
-import User from "../../server/schemas/user-schema.js";
-
-export const name = "vitoria";
-export async function execute(message) {
-  message.reply("oi");
-}
